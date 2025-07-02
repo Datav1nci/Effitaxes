166140eacd519161c2925d9357bb533c33a65107
Datav1nci <jestais4@gmail.com>
2025-07-02 10:29:17 -0400

    commented out the image import in header

--- effitaxes/src/components/Header.tsx
+++ effitaxes/src/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Image from 'next/image';
+//import Image from 'next/image';
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Moon, Sun, Menu, X } from "lucide-react";
