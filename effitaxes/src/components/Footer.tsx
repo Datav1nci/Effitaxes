f0b5efc68410f09429ba205a01af202aa2bd1a8f
Datav1nci <jestais4@gmail.com>
2025-12-25 22:28:31 -0500

    feat: add basic Footer component

--- effitaxes/src/components/Footer.tsx
+++ effitaxes/src/components/Footer.tsx
@@ -2,9 +2,9 @@ export default function Footer() {
   return (
     <footer className="bg-gray-50 text-slate-700 py-10 border-t border-gray-200 dark:bg-gray-950/40 dark:text-gray-300 dark:border-gray-800">
       <div className="mx-auto max-w-7xl px-4 flex flex-col items-center justify-center text-center">
-        <h3 className="text-2xl font-promethean text-slate-900 dark:text-white">
+        <h6 className="text-2xl font-promethean text-slate-900 dark:text-white">
           © 2025 Effita{"><"}es Tous droits réservés.
-        </h3>
+        </h6>
       </div>
     </footer>
   );
